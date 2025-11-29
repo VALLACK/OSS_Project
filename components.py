ba75240bb3c0e2035851cfc60f783306c2064d1a
Nam Tae Uk <nam20913@gmail.com>
2025-11-29 15:32:46 +0900

    implement toggle_flag

--- components.py
+++ components.py
@@ -137,11 +137,13 @@ class Board:
         self._check_win()
 
     def toggle_flag(self, col: int, row: int) -> None:
-        # TODO: Toggle a flag on a non-revealed cell.
-        # if not self.is_inbounds(col, row):
-        #     return
-        
-        pass
+        if not self.is_inbounds(col, row):
+            return
+        idx = self.index(col, row)
+        cell = self.cells[idx]
+        if cell.state.is_revealed:
+            return
+        cell.state.is_flagged = not cell.state.is_flagged
 
     def flagged_count(self) -> int:
         # TODO: Return current number of flagged cells.
